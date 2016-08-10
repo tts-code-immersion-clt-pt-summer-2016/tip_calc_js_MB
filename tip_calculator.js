29f160bac5be6ab09d88558bc41b65a5cfc20bf5
Mike Bechtel <mikebec20@gmail.com>
2016-08-09 22:20:22 -0400

    Testing Numeral.js
    testing this library from http://numeraljs.com/

--- tip_calculator.js
+++ tip_calculator.js
@@ -1,5 +1,5 @@
 var bill_amount = prompt("Please Enter Your Total Bill Amount:");
-var tip_amount = Number(bill_amount) * 0.20;
-var total_amount = Number(bill_amount) + Number(tip_amount);
+var tip_amount = Numeral(bill_amount * 0.20).format('$0,0.00');
+var total_amount = Numeral(bill_amount + tip_amount).format('$0,0.00');
 
-alert("Bill Amount: $" + bill_amount + "\nTip Amount: $" + tip_amount + "\nTotal Amount Due: $" + total_amount);
+alert("Bill Amount: " + bill_amount + "\nTip Amount: " + tip_amount + "\nTotal Amount Due: " + total_amount);
